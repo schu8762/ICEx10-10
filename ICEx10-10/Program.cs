9bf0732499755a1e553421776cbc0ba0bfa41527
schu8762 <J.Schuller@ou.edu>
2019-10-10 10:34:58 -0500

    in class initial

--- ICEx10-10/Program.cs
+++ ICEx10-10/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//@author Jacob Schuller
 namespace ICEx10_10
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { //Functions 10/10/2019
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
